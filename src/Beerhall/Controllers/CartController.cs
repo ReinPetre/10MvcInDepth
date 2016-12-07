24c467cc115cf1fa372063c0a2ef56899d44094f
Stefaan Samyn <stefaan.samyn@hogent.be>
2016-12-07 01:50:32 +0100

    Implement Checkout - Get

--- src/Beerhall/Controllers/CartController.cs
+++ src/Beerhall/Controllers/CartController.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Beerhall.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Beerhall.Models.Domain;
 using Beerhall.Models.ViewModels.CartViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Beerhall.Controllers {
     [ServiceFilter(typeof(CartSessionFilter))]
@@ -49,8 +51,12 @@ namespace Beerhall.Controllers {
             return RedirectToAction("Index");
         }
 
+        [Authorize(Policy = "Customer")]
         public IActionResult Checkout(Cart cart) {
-            throw new NotImplementedException();
+            if (cart.NumberOfItems == 0)
+                return RedirectToAction("Index", "Store");
+            IEnumerable<Location> locations = _locationRepository.GetAll().OrderBy(l => l.Name).ToList();
+            return View(new CheckOutViewModel(locations, new ShippingViewModel()));
         }
     }
 }
